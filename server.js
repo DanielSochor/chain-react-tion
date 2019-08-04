855e9c2c79978ffa54ef3cb3b2babdb53bf99aab
Daniel Sochor <danielsochor@gmail.com>
2019-08-03 23:30:51 -0500

    fixes to make package backend work

--- server.js
+++ server.js
@@ -32,9 +32,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../healthy-front-end/build"));
 }
-
 
 //path.join returns a normalized path by merging two paths together
 //__dirname gives you the path of the currently running file.
@@ -51,7 +50,7 @@ require("./controllers/routes")(app);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../healthy-front-end/build/index.html"));
 });
 
 // app.get('/', function(req, res, next) {
